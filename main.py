c3ef22332817d1203b26f899d441349d7ac1c44b
Faran Masood Peerzada <faranpeerzada253@gmail.com>
2023-03-09 23:35:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ BACKGROUND_COLOR = "#B1DDC6"
 current_card={}
 to_learn={}
 try:
-    data= pd.read_csv("data/words_to_learn.csv")
+    data= pd.read_csv("words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pd.read_csv("data/french_words.csv")
+    original_data = pd.read_csv("french_words.csv")
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn=data.to_dict(orient="records")
 def is_known():
     to_learn.remove(current_card)
     data= pd.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
+    data.to_csv("words_to_learn.csv", index=False)
     next_card()
 def next_card():
     global current_card,flip_timer
@@ -42,8 +42,8 @@ window.config(padx=50,pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(3000, func=flip_card )
 
 canvas= Canvas(width=800, height=526)
-card_front=PhotoImage(file="images/card_front.png")
-card_back = PhotoImage(file="images/card_back.png")
+card_front=PhotoImage(file="card_front.png")
+card_back = PhotoImage(file="card_back.png")
 card_background=canvas.create_image(400,263,image=card_front)
 card_title=canvas.create_text(400,150, text="title", font=('Ariel',40,'italic'))
 card_word=canvas.create_text(400,263, text="word", font=('Ariel',50,'bold'))
@@ -51,13 +51,13 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0,row=0, columnspan=2)
 
 #Buttons
-cross_image=PhotoImage(file="images/wrong.png")
+cross_image=PhotoImage(file="wrong.png")
 unknown_button=Button(image=cross_image, highlightthickness=0, command=next_card)
 #unknown_button.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 unknown_button.grid(column=0,row=1)
 
 
-tick_image=PhotoImage(file="images/right.png")
+tick_image=PhotoImage(file="right.png")
 known_button=Button(image=tick_image, highlightthickness=0, command=is_known)
 #unknown_button.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 known_button.grid(column=1,row=1)
